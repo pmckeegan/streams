6d9d4fdea73204341d61ba4d52245371b49ed71a
pmckeegan <pmckeegan@gmail.com>
2019-03-11 04:58:03 +0000

    added an https callback

--- https_example.js
+++ https_example.js
@@ -2,3 +2,18 @@ var https = require('https');
 
 console.log('I did it!');
 
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function() {
+  console.log("in response handler callback!");
+
+}
+
+console.log("i'm about to make the request!");
+
+https.request(options, callback).end();
+console.log("I've made the request!");
+
